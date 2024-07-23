b6dc2243c317bc26f1d31e4ab7000d8ebe987c86
Саликова Мария Тимуровна <salikova.work@gmail.com>
2024-07-23 12:43:41 +0300

    Authentication implied

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -4,10 +4,21 @@ import (
 	"github.com/mariiasalikova/todo-app/pkg/repository"
 	"github.com/mariiasalikova/todo-app"
 	"crypto/sha1"
+	"github.com/dgrijalva/jwt-go"
 	"fmt"
+	"time"
 )
 
-const salt = "dfyuiopoejw73hdd9o"
+const (
+	salt = "dfyuiopoejw73hdd9o"
+	signingKey = "HiduIFY&*YWGEU$ED"
+	tokenTTL = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -23,6 +34,22 @@ func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	return s.repo.CreateUser(user)
 }
 
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+		ExpiresAt: time.Now().Add().Unix(),
+		IssuedAt: time.Now().Unix(),
+	},
+	user.Id
+	})
+
+	return token.SignedString()
+}
 
 func (s *AuthService) generatePasswordHash(password string) string {
 	hash := sha1.New()
